1671c82b6845614f156c5012199d0296345098e8
muhammad jamil <dinalyjamil@gmail.com>
2024-05-30 05:04:41 +0500

    changes in medicine page

--- src/feature/cartslicer.js
+++ src/feature/cartslicer.js
@@ -1,7 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  cart: [],
-};
-const cartProduct = createSlice({ name: cart, initialState, reducers: {} });
-export default cartProduct.reducer;
+// const initialState = {
+//   cart: [],
+// };
+// const cartProduct = createSlice({ name: cart, initialState, reducers: {} });
+// export default cartProduct.reducer;
